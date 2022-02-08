68cfbe453e4e1e55fcfd91d0123e3074bb8d80bf
evans <opiloevans96@gmail.com>
2022-02-08 16:48:59 +0300

    added url validator test

---
+++ pkg/validator/url_validate_test.go
@@ -0,0 +1,23 @@
+package validator
+
+import "testing"
+
+func TestValidate(t *testing.T) {
+	type args struct {
+		URL string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Validate(tt.args.URL); (err != nil) != tt.wantErr {
+				t.Errorf("Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
